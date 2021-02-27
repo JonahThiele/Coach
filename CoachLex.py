7b8cc9abac9d0336cff47f7530842cb790d13d6e
Derplz21 <56401646+Derplz21@users.noreply.github.com>
2021-02-27 14:43:57 -0600

    Add files via upload

--- CoachLex.py
+++ CoachLex.py
@@ -22,7 +22,9 @@ tokens = (
     'OUTPUT',
     'LESSTHAN',
     'GREATERTHAN',
-    'FUNC'
+    'FUNC',
+    #'NAME',
+    'EQUALTO'
     )
 
 # regular expressions basic definitions
@@ -30,19 +32,21 @@ def t_ADDA(t): r'laps'; return t
 def t_ADDB(t): r'have'; return t
 def t_ADDC(t): r'increased'; return t
 def t_ADDD(t): r'by:'; return t
-def t_SUBA(t): r'completed'; return t
+def t_SUBA(t): r'finished'; return t
 def t_SUBB(t): r'out'; return t
 def t_SUBC(t): r'of'; return t
 def t_DIV(t): r'split'; return t
 def t_MULT(t): r'by'; return t
 def t_INCREMENT(t): r'Jaccoby'; return t
-def t_VARINT(t): r'laps'; return t
+def t_VARINT(t): r'lap'; return t
 t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
+#t_NAME = r'[a-zA-Z_][a-zA-Z0-9_ ]*'
 t_TRUE = r'TRUE'
 t_FALSE = r'FALSE'
-def t_OUTPUT(t): r'Coach;'; return t
+def t_OUTPUT(t): r'Coach:'; return t
 t_LESSTHAN = r'<'
 t_GREATERTHAN = r'>'
+t_EQUALTO = r'='
 def t_IF(t): r'if'; return t
 def t_WHILE(t): r'while'; return t
 def t_FUNC(t): r'workout'; return t
--- CoachYacc.py
+++ CoachYacc.py
@@ -12,9 +12,15 @@ def p_statement_expr(p):
     'statement : expression'
     print(p[1])
 
-def p_statement_output(p):
-    'statement : OUTPUT expression'
-    print("Coach says" + str(p[2]) + "!")    
+#def p_statement_output(p):
+   # 'statement : OUTPUT NAME'
+   # print("Coach says " + str(p[2]) + "!")
+
+#def p_statement_if(p):
+ #   '''statement : IF expression GREATERTHAN expression
+  #                 IF expression LESSTHAN expression
+  #                 IF expression EQUALTO expression'''
+  #  if p[3] == '>': 
     
 def p_expression_basicop(p):
     '''expression : expression ADDA ADDB ADDC ADDD expression
@@ -22,7 +28,7 @@ def p_expression_basicop(p):
                   | expression MULT expression
                   | expression DIV expression'''
     if p[2] == 'laps' and p[3] == 'have' and p[4] == 'increased' and p[5] == 'by:': p[0] = p[1] + p[6]
-    elif p[2] == 'completed' and p[3] == 'out' and p[4] == 'of': p[0] = p[5] - p[1]
+    elif p[2] == 'finished' and p[3] == 'out' and p[4] == 'of': p[0] = p[5] - p[1]
     elif p[2] == 'by': p[0] = p[1] * p[3]
     elif p[2] == 'split': p[0] = p[1] / p[3]
 
