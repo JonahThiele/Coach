8bf468a234fbcb8337a935b1f4b7ae360bfb3880
crabster15 <j715crab@gmail.com>
2021-02-26 20:40:31 -0600

    initial commit

---
+++ Coach++Lex.py
@@ -0,0 +1,26 @@
+import lex 
+
+# all of our tokens
+tokens = (
+    'NUMBER',
+    'VAR',
+    'MULT',
+    'DIV',
+    'ADD',
+    'SUB',
+    'INCREMENT',
+    'TRUE',
+    'FALSE',
+    'WHILE',
+    'IF',
+    'OUTPUT',
+    'LESSTHAN',
+    'GREATERTHAN',
+    'FUNC'
+    )
+
+# regular expressions
+t_ADD    = r'number to go'
+t_SUB   = r'completed'
+t_DIV  = r'splits'
+t_INCREMENT = r'Jaccoby'
